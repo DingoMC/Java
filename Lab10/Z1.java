5744aad37f8873d64456f222061145d50b03614e
DingoMC <95879619+DingoMC@users.noreply.github.com>
2023-11-25 19:09:02 +0100

    Create Z1.java

---
+++ Lab10/Z1.java
@@ -0,0 +1,41 @@
+package lab10.z1;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://cs.pollub.pl/staff/").get();
+
+        doc.select("p").forEach((element) -> {
+            Element sibling = element.previousElementSibling();
+            System.out.println(sibling.text());
+            Map<String, String> m = new HashMap<>();
+            element.select("a").forEach((element1) -> {
+                String text = element1.text();
+                String cut_text = text.replaceAll("dr", "")
+                        .replaceAll("mgr", "")
+                        .replaceAll("inż.", "").
+                        replaceAll(", prof. uczelni", "");
+                String[] sep = cut_text.split(" ");
+                m.put(text, sep[sep.length - 1]);
+            });
+            List<Map.Entry<String, String>> l = new ArrayList<>(m.entrySet());
+            l.sort(Map.Entry.comparingByValue());
+            for (Map.Entry<String, String> entry : l) {
+                System.out.println(entry.getKey());
+            }
+            System.out.println();
+        });
+    }
+}
